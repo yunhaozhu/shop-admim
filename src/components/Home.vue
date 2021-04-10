bcb4b81c12430262085ff4ae0ea22242da623284
Wilson <quantumfram@gmail.com>
2021-04-10 22:13:04 +0800

    Navigation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,111 @@
 <template>
-    <div>
-      Home
-      <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+      <el-header>
+        <div>
+          <span>Dashboard</span>
+        </div>
+        <el-button size="medium" plain @click="logout">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside :width="isCollapse?'64px':'230px'">
+          <el-menu
+            :collapse="isCollapse"
+            unique-opened router
+            background-color="#f5f8fa"
+            text-color="#33475b"
+            active-text-color="#019eae"
+            :collapse-transition="false">
+            <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+              <template slot="title">
+                <i :class="iconsObj[item.id]"></i>
+                <span>{{item.authName}}</span>
+              </template>
+              <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                  <span>{{subItem.authName}}</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+            <div class="toggle-button" @click="toggleCollapse">|||</div>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      menuList: [],
+      iconsObj: {
+        125: 'el-icon-user-solid',
+        103: 'el-icon-s-tools',
+        101: 'el-icon-s-goods',
+        102: 'el-icon-s-order',
+        145: 'el-icon-s-platform'
+      },
+      isCollapse: false
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .home-container{
+    height: 100%;
+  }
+  .el-menu{
+    height: 100%;
+  }
+  .el-header{
+    background-color: #2e3f50;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #ffffff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+    }
+  }
+  .el-aside{
+    background-color: #f5f8fa;
+  }
+  .el-main{
+    background-color: #ffffff;
+    //#ff7a59 #eaf0f6 #e5f5f8
+  }
+  .toggle-button {
+    background-color: #f5f8fa;
+    line-height: 24px;
+    font-size: 10px;
+    color: #33475b;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
 </style>
